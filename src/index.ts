679b94b25d31ccdcfbdbb99e95fabc00627af03e
Artur Abdullin <artur.abdullin@gmail.com>
2022-11-05 23:11:38 +0300

    feat: add github-link

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
 import chuckImg from "./assets/chuck.png";
+import gitHubIcon from "./assets/github.svg";
 
 const laughImg: HTMLImageElement = document.getElementById(
   "laughImg"
 ) as HTMLImageElement;
 laughImg.src = chuckImg;
+
+const gitHubImg: HTMLImageElement = document.querySelector('.github-link__img') as HTMLImageElement;
+gitHubImg.src = gitHubIcon;
 
 generateJoke();
 
